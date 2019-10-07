4426ac4ba0b8f8d6682e5d0bd1174fa52c0e7a02
Daria Hauer <dariahauer2@gmail.com>
2019-10-07 21:36:01 +0200

    finding answear how to do the correct boolen methon

---
+++ src/RightTriangleTest.java
@@ -0,0 +1,28 @@
+public class RightTriangleTest {
+
+//    public  void isRightTriangle (double a, double b, double c){
+//        double calc = (((a*a)+(b*b)=(c*c))||(a*a+c*c=b*b)||(c*c+b*b=a*a));
+//        return
+//
+//        boolean rightTriangle
+   public boolean isRightTriangle (Triangle triangle){
+       double calc = (Math.pow)
+
+               ((triangle.sideA*triangle.sideA)+(triangle.sideB*triangle.sideB)==(triangle.sideC*triangle.sideC));
+       boolen result ==
+       return calc ==;
+
+
+
+   }
+}
+
+//
+//    boolean isEven(int value) {
+//        int mod = value % 2;
+//        return mod == 0;
+//    }
+//
+//    boolean isOdd(int value) {
+//        return isEven(value);
+//    }
